877abd9ec8a213d73ef7f4b2840bb0127b3044ee
Billy <billy@alcachofa.uk>
2026-08-05 21:28:07 +0000

    Fix workspace migration parent

--- backend/migrations/versions/a9d4e7c2b1f0_add_workspace_import_target.py
+++ backend/migrations/versions/a9d4e7c2b1f0_add_workspace_import_target.py
@@ -1,7 +1,7 @@
 """add workspace import target
 
 Revision ID: a9d4e7c2b1f0
-Revises: f4c1a7b2e9d0
+Revises: ac35ed854b83
 Create Date: 2026-08-05 21:35:00.000000
 """
 
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "a9d4e7c2b1f0"
-down_revision = "f4c1a7b2e9d0"
+down_revision = "ac35ed854b83"
 branch_labels = None
 depends_on = None
 
