eaef83f1f09c1a34a186d70047b726f0d82bc50e
Edward Salkeld <edsalkeld@alcachofa.faith>
2026-09-08 21:49:17 +0100

    Fix Nix npm dependency hash for Vitest update

--- flake.nix
+++ flake.nix
@@ -109,7 +109,7 @@
             pname = "wantlist-frontend";
             version = "0.1.0";
             src = ./frontend;
-            npmDepsHash = "sha256-nQwHvZAByd5Eg7Fe/p8nujBL2AcJnGtYserYw7k3MaE=";
+            npmDepsHash = "sha256-XfhBfrpafC6IhC6lLhV2ZSFJQTtmTT5fsFk37OLCNSs=";
             npmBuildScript = "build";
             installPhase = ''
               runHook preInstall
